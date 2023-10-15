8cb7bffa6e02ca30ea970ab8028cca90fb46623d
Cristiano M <devcristianomorais@gmail.com>
2023-10-14 23:49:14 -0300

    added method send typing

--- src/BotLauncher.java
+++ src/BotLauncher.java
@@ -1,18 +1,26 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.EnumSet;
 
+/**
+ * @author Cristiano M
+ * @created 2023-10-11
+ * @modifield 2023-10-14
+ */
+
 public class BotLauncher extends ListenerAdapter {
 
     public static JDA jda;
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException {
 
         jda = JDABuilder.create(Config.KEY_TOKEN, EnumSet.allOf(GatewayIntent.class)).build();
+        jda.getPresence().setPresence(Activity.customStatus("Versão 0.0.2"), true);
 
         jda.addEventListener(new Commands());
-
+        jda.awaitReady();
     }
 }
--- src/Commands.java
+++ src/Commands.java
@@ -1,14 +1,23 @@
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * @author Cristiano M
+ * @created 2023-10-11
+ * @modifield 2023-10-14
+ */
+
 public class Commands extends ListenerAdapter {
+
+    private final String PREFIX = "!";
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
         String[] args = event.getMessage().getContentRaw().split(" ");
 
-        if(args[0].equalsIgnoreCase("!" + "teste")){
+        if(args[0].equalsIgnoreCase(PREFIX + "teste")){
+            event.getChannel().sendTyping().queue();
             event.getMessage().reply("Teste feito com sucesso!").queue();
         }
     }
